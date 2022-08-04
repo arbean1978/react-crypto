10ff018a4c9177056aa63a51d56ae972b1ed0900
Aaron Bean <arbean1978@gmail.com>
2022-08-04 12:59:30 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React from 'react'
-import Featured from './components/Featured'
-import Hero from './components/Hero'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Routes, Route } from 'react-router-dom'
+import Coins from './components/Coins'
+import Coin from './routes/Coin'
 import Navbar from './components/Navbar'
-import Signup from './components/Signup'
-import Footer from './components/Footer'
+
 
 function App() {
+
+  const [coins, setCoins] = useState([])
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data)
+      // console.log(response.data[0])
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
   return (
     <>
       <Navbar />
-      <Hero />
-      <Featured />
-      <Signup />
-      <Footer />
+      <Routes>
+        <Route path='/' element={<Coins coins={coins} />} />
+        <Route path='/coin' element={<Coin />}>
+          <Route path=':coinId' element={<Coin />} />
+        </Route>
+      </Routes>
+
     </>
   );
 }
